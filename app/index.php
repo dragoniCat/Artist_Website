42333db93e18d102370ff93377c217422b084311
dragoniCat <92878236+dragoniCat@users.noreply.github.com>
2024-08-01 14:54:54 +0200

    ???

--- app/index.php
+++ app/index.php
@@ -2,16 +2,29 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title>Page Title</title>
-        <meta name="description" content="Roughly 155 characters">
-        <link rel="stylesheet" type="text/css" href="./view/style.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Artist Website</title>
+        <meta name="description" content="This is a website intended to showcase an artist's work">
+
+        <!-- Bootstrap CSS -->
+        <link 
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
+            rel="stylesheet" 
+            integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
+            crossorigin="anonymous"
+        >
+        <!-- Custom CSS -->
+        <link rel="stylesheet" type="text/css" href="src/view/style.css">
     </head>
     <body>
         <!-- Content -->
-
-        <?php
-            include("src/view/header.html");
-        ?>
+        <div class="window">
+            <?php include "src/view/components/header.php" ?>
+            <div class="row gx-0">
+                <?php include "src/view/components/sidebar.html" ?>
+                <?php include "src/view/components/main-area.html" ?>
+            </div>
+            <?php include "src/view/components/footer.html" ?>
+        </div>
     </body>
 </html>
---
+++ app/src/view/components/header.php
@@ -0,0 +1,18 @@
+<header class="header">
+    <div class="row">
+        <div class="col-1">
+            <?php include "./././assets/logo.html" ?>
+        </div>
+        <div class="col">
+            2
+        </div>
+        <div class="col">
+            <div class="btn-group d-flex" role="group" aria-label="button group"
+            style="height: 100%;">
+                <button type="button" class="btn btn-primary">Button</button>
+                <button type="button" class="btn btn-primary">Button</button>
+                <button type="button" class="btn btn-primary">Button</button>
+            </div>
+        </div>
+    </div>
+</header>
